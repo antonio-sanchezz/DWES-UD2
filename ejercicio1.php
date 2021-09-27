e85a111d60f7023caf8033d64371ccb72c29c774
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-09-27 12:11:07 +0200

    Ejercicio 1

--- ejercicio1.php
+++ ejercicio1.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 01</title>
+</head>
+<body>
 <?php
 
 $nombre = "Antonio Sanchez Espinosa";
@@ -9,3 +18,5 @@ echo "Soy " . $nombre . ", tengo " . $edad . " años de edad y vivo en " . $pobl
 printf("Soy %s, tengo %d años de edad y vivo en %s.", $nombre, $edad, $poblacion);
 
 ?>
+</body>
+</html>
