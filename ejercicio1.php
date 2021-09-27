9de49fcd4f02ce7d27d096484a2806366c083bd2
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-09-27 12:00:35 +0200

    Ejercicio 1

---
+++ ejercicio1.php
@@ -0,0 +1,11 @@
+<?php
+
+$nombre = "Antonio Sanchez Espinosa";
+$poblacion = "Sevilla";
+$edad = 23;
+
+echo "Soy " . $nombre . ", tengo " . $edad . " años de edad y vivo en " . $poblacion . ".<br />";
+
+printf("Soy %s, tengo %d años de edad y vivo en %s.", $nombre, $edad, $poblacion);
+
+?>
