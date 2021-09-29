268bc7b834c283e61a6b553d3d32b4e157fbebca
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-09-29 11:46:16 +0200

    08

---
+++ ejercicio8.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$text = "Mensaje Secreto";
+
+echo(gettype($text));
+echo "<br>";
+echo(is_string($text));
+echo "<br>";
+echo(is_numeric($text));
+echo "<br>";
+echo "<hr>";
+//--------------------------
+$text = 23.52;
+
+echo(gettype($text));
+echo "<br>";
+echo(is_string($text));
+echo "<br>";
+echo(is_numeric($text));
+echo "<br>";
+echo "<hr>";
+//--------------------------
+
+echo((int)$text);
+echo "<br>";
+
+$newVar = "6dofaiojasd";
+
+echo $newVar;
+echo "<br>";
+echo((int)$newVar);
+
+
+?>
+</body>
+</html>
