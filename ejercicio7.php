8d53b15092f3711f9d4131a393207cb04ecabf0a
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-09-29 10:46:26 +0200

    07

---
+++ ejercicio7.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$num1 = 3545;
+$num2 = 55641;
+
+printf("El valor en binario de la variable 1 es %b <br>", $num1);
+printf("El valor en binario de la variable 2 es %b <br>", $num2);
+printf("El resultado de la operación de comparación bit a bit AND es %b", $num1&$num2);
+
+?>
+</body>
+</html>
